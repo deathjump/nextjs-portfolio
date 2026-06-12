9385836755c5a25a500979586fa63914202b4b0c
deathjump <semekor.2005@gmail.com>
2026-06-12 04:56:51 +0000

    encoding disabled

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Prevents npm packages from crashing if they look for 'encoding'
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        encoding: false,
+      };
+    }
+    return config;
+  },
+}
 
 module.exports = nextConfig
